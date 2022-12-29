60a2a0ae0c2fe43401248d104db9b88e3b100e39
Carl <qwerfcxzas1@gmail.com>
2022-12-29 22:39:29 +0800

    路由

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/foo', function () {
+    return "Hello World!";
+});
+//多個請求
+Route::match(['get', 'post'], '/match', function () {
+    dd("match");
+});
+//重定向
+Route::redirect('/foo', '/match', 301);
+//視圖路由
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+//必填参数
+Route::get('user/{id}', function ($id) {
+    return $id;
+})->where('id', '[0-9]+');
+//可選參數
+Route::get('article/{id?}', function ($id = 0) {
+    return $id;
+});
+//路由命名
+Route::get('user/profile', function () {
+    $url = route('profile');
+    dd($url);
+})->name('profile');
+
+
+//帶參數的路由命名
+Route::get('user/{id}/profile1', function ($id) {
+    //只有一個參數，可以直接寫
+    $url = route('user.profile1', $id);
+    dd($url);
+})->name('user.profile1');
+//帶參數的路由命名,多個參數
+Route::get('user/{id}/{test}/profile', function ($id,$test) {
+    //第二個參數傳遞路由參數
+    $url = route('user.profile', ['id' => $id,'test'=>$test]);
+    dd($url);
+})->name('user.profile');
+
+
